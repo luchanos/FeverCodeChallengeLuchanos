3f8034f9de3aba5d82b7fb95e9174bc7f87ff1e9
luchanos <lukash_91@mail.ru>
2023-02-19 19:26:36 +0300

    [upd] linters

--- db/models.py
+++ db/models.py
@@ -1,28 +1,43 @@
+import datetime
 import uuid
 
+from sqlalchemy import Boolean
 from sqlalchemy import Column
 from sqlalchemy import DateTime
-from sqlalchemy import Float
+from sqlalchemy import ForeignKey
 from sqlalchemy import String
 from sqlalchemy import UniqueConstraint
+from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
 
+class BaseEvent(Base):
+    __tablename__ = "base_events"
+
+    base_event_id = Column(String, primary_key=True)
+    sell_mode = Column(String)
+    title = Column(String)
+    is_active = Column(Boolean, default=True)
+    last_updated_dt = Column(DateTime(timezone=True), default=datetime.datetime.now())
+
+
 class Event(Base):
     __tablename__ = "events"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    base_event_id = Column(String)
-    title = Column(String)
-    price = Column(Float)
     event_start_date = Column(DateTime(timezone=True))
     event_end_date = Column(DateTime(timezone=True))
-    zone_id = Column(String)
+    event_id = Column(String)
+    base_event_id = Column(String, ForeignKey("base_events.base_event_id"))
+    is_active = Column(Boolean, default=True)
+    zones = Column(JSONB)
+    last_updated_dt = Column(DateTime(timezone=True), default=datetime.datetime.now())
+
     __table_args__ = (
         UniqueConstraint(
-            "base_event_id", "zone_id", "price", name="base_event_id_zone_id_price_idx"
+            "base_event_id", "event_id", name="base_event_id_event_id_idx"
         ),
     )
--- worker/event_worker.py
+++ worker/event_worker.py
@@ -1,22 +1,18 @@
 import asyncio
+import datetime
 import pathlib
-from copy import deepcopy
 
+from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 import settings
-from db.dals import EventDAL
+from db.models import BaseEvent
+from db.models import Event
 from worker.models import eventList
 
-
-class VendorClient:
-    def __init__(self, base_url: str):
-        self.base_url = base_url
-
-    async def get_events(self):
-        pass
+# import aiohttp
 
 
 engine = create_async_engine(
@@ -28,38 +24,81 @@ engine = create_async_engine(
 
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
+APPROVED_SELL_MODE = [
+    "online",
+]
 
-def make_events(base_events) -> list:
-    events = []
-    for base_event in base_events:
-        new_event = {
-            "title": base_event.title,
-            "base_event_id": base_event.base_event_id,
-        }
-        for event in base_event.events:
-            new_event["event_start_date"] = event.event_start_date
-            new_event["event_end_date"] = event.event_end_date
-            for zone in event.zones:
-                new_event["price"] = zone.price
-                new_event["zone_id"] = zone.zone_id
-                events.append(deepcopy(new_event))
-    return events
+
+def create_update_base_events_query(base_events):
+    return (
+        insert(BaseEvent)
+        .values(
+            [
+                dict(
+                    base_event_id=base_event.base_event_id,
+                    sell_mode=base_event.sell_mode,
+                    title=base_event.title,
+                )
+                for base_event in base_events
+                if base_event.sell_mode in APPROVED_SELL_MODE
+            ]
+        )
+        .on_conflict_do_update(
+            index_elements=[BaseEvent.base_event_id],
+            set_=dict(last_updated_dt=datetime.datetime.now()),
+        )
+    )
+
+
+def create_update_events_query(event, base_event_id):
+    # todo если у нас что-то упадет по одному, то получится, что завалится все сразу
+    return (
+        insert(Event)
+        .values(
+            [
+                dict(
+                    event_start_date=event.event_start_date,
+                    event_end_date=event.event_end_date,
+                    event_id=event.event_id,
+                    base_event_id=base_event_id,
+                    zones=[x.dict() for x in event.zones],
+                )
+            ]
+        )
+        .on_conflict_do_update(
+            index_elements=[Event.base_event_id, Event.event_id],
+            set_=dict(last_updated_dt=datetime.datetime.now()),
+        )
+    )
 
 
 async def main():
     # async with aiohttp.ClientSession() as session:
     #     async with session.get('https://provider.code-challenge.feverup.com/api/events', ssl=False) as resp:
     #         xml_from_resp = await resp.text()
-    #         modeled = eventList.from_xml(xml_from_resp)
+    #         event_list = eventList.from_xml(xml_from_resp)
 
     xml_doc = pathlib.Path("response.xml").read_text()
-    modeled = eventList.from_xml(xml_doc)
+    event_list = eventList.from_xml(xml_doc)
 
     async with async_session() as db_session:
         async with db_session.begin():
-            event_dal = EventDAL(db_session=db_session)
-            events_list = make_events(modeled.output.base_events)
-            await event_dal.write_events_to_database(events_list)
+            # todo luchanos add chunks
+            update_base_events_query = create_update_base_events_query(
+                event_list.output.base_events
+            )
+            await db_session.execute(update_base_events_query)
+            for base_event in event_list.output.base_events:
+                if (
+                    base_event.sell_mode not in APPROVED_SELL_MODE
+                ):  # todo тут можно прописывать все типы и не делать проверку - делать только на выдаче
+                    continue
+                base_event_id = base_event.base_event_id
+                for event in base_event.events:
+                    update_events_query = create_update_events_query(
+                        event, base_event_id
+                    )
+                    await db_session.execute(update_events_query)
 
 
 asyncio.run(main())
